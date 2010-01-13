56ee48b629af5b7cd0e4020c622bd07b298d2d5a
lolautruche <info@example.com>
2010-01-13 09:48:38 +0000

    1.0 branch creation
    git-svn-id: http://svn.projects.ez.no/jvamf@16 cc6b4b78-d1ee-4958-abc2-b1844ac0a70e

---
+++ branches/1.0/ezinfo.php
@@ -0,0 +1,44 @@
+<?php
+//
+// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+// SOFTWARE NAME: jvAMF - AMF Connector for eZ Publish
+// SOFTWARE RELEASE: @@@VERSION@@@
+// COPYRIGHT NOTICE: Copyright (C) 2009 Jerome Vieilledent
+// SOFTWARE LICENSE: GNU General Public License v2.0
+// NOTICE: >
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of version 2.0  of the GNU General
+//   Public License as published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of version 2.0 of the GNU General
+//   Public License along with this program; if not, write to the Free
+//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//   MA 02110-1301, USA.
+//
+//
+// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+//
+
+class jvAMFInfo
+{
+    static function info()
+    {
+        return array( 'Name'      => 'jvAMF',
+                      'Version'   => '@@@VERSION@@@',
+                      'Copyright' => 'Copyright © 2009 Jérôme Vieilledent',
+                      'Info'      => '<a href="http://projects.ez.no/jvamf" target="_blank">http://projects.ez.no/jvamf</a>',
+                      'License'   => 'GNU General Public License v2.0',
+                      'Includes the following library'              => array( 'Name' => 'Zend_AMF (part of Zend Framework)',
+                                                                              'Version' => '1.9.7',
+                                                                              'Copyright' => 'Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)',
+                                                                              'License' => 'New BSD License : http://framework.zend.com/license/new-bsd',),
+                    );
+    }
+}
+
+?>
---
+++ branches/1.0/modules/jvamf/endpoint.php
@@ -0,0 +1,64 @@
+<?php
+//
+// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+// SOFTWARE NAME: jvAMF - AMF Connector for eZ Publish
+// SOFTWARE RELEASE: @@@VERSION@@@
+// COPYRIGHT NOTICE: Copyright (C) 2009 Jerome Vieilledent
+// SOFTWARE LICENSE: GNU General Public License v2.0
+// NOTICE: >
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of version 2.0  of the GNU General
+//   Public License as published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of version 2.0 of the GNU General
+//   Public License along with this program; if not, write to the Free
+//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//   MA 02110-1301, USA.
+//
+//
+// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+//
+
+/**
+ * Handle the AMF service call
+ */
+
+eZDebug::createAccumulator('Zend_AMF Configuration', 'jvAMF');
+eZDebug::accumulatorStart('Zend_AMF Configuration');
+
+// Zend lib must be in the include path to work
+set_include_path('extension/jvamf/lib'.PATH_SEPARATOR.get_include_path());
+
+$ini = eZINI::instance('jvamf.ini');
+$aServicesDirs = $ini->variable('AMF', 'ServicesDir');
+$aClassMapping = $ini->variable('AMF', 'ClassMap');
+$productionMode = $ini->variable('AMF', 'ProductionMode') == 'enabled' ? true : false;
+
+$server = new Zend_Amf_Server();
+$server->setProduction($productionMode);
+
+// Les repertoires de services
+foreach($aServicesDirs as $dir)
+{
+	$server->addDirectory($dir);
+}
+
+// Le ClassMapping
+foreach($aClassMapping as $asClass => $phpClass)
+{
+	$server->setClassMap($asClass, $phpClass);
+}
+
+eZDebug::accumulatorStop('Zend_AMF Configuration');
+
+$AMFresult = $server->handle();
+echo $AMFresult;
+
+// Clean exit to avoid debug to display
+eZDB::checkTransactionCounter();
+eZExecution::cleanExit();
